1e22f48a4db7505b3641a91fc843cb33ed02d5a7
mstpierre73 <naranjaqc@hotmail.com>
2019-05-28 09:20:03 -0400

    "add React state project"

---
+++ app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello from my app!</h1>
+            </div>
+        )
+    }
+} 
+
+ReactDOM.render(<App />, document.getElementById("root"));

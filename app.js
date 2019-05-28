ddbf4af99c295e1ddbf386d2def53a95c85f4fd5
mstpierre73 <naranjaqc@hotmail.com>
2019-05-28 10:06:40 -0400

    "add function and state changes to project"

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 class App extends React.Component {
+    state = {
+        name: "Ryu",
+        age: 35
+    }
+
+    //When we use arrow function, the context of this is the component
+    handleClick = (event) => {
+        console.log(event.target);
+        this.setState({
+            name: "Yoshi",
+            age: 25
+        });
+        console.log(this.state)
+    }
+
+    handleMouseOver = (e) => {
+        console.log(e.target, e.pageX);
+    }
+
+    handleCopy  = (e) => {
+        console.log("Try being original for once!");
+    }
+
     render(){
         return(
             <div>
                 <h1>Hello from my app!</h1>
+                <p>Hi! My name is : {this.state.name} and my age is: {this.state.age}</p>
+                <button onClick={this.handleClick}>Click Me!</button>
+                <button onMouseOver={this.handleMouseOver}>Hover Me!</button>
+                <p onCopy={this.handleCopy}>What we think, we become</p>
             </div>
         )
     }
